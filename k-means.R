c00ca23a6756a67e4fead3f4a03e368dc233952f
Jess <77901330+jjesss@users.noreply.github.com>
2023-10-08 20:34:03 +0100

    Update k-means.R

--- k-means.R
+++ k-means.R
@@ -1,18 +1,14 @@
-#################################################################################################
-## Image compression using k-means clustering ##
-#################################################################################################
-
-# install.packages("jpeg")
-# install.packages("gridExtra")
-# install.packages("ggplot2")
-
 library(jpeg)
 library(ggplot2)
 library(grid)
 require(gridExtra)
 
 # Load Image.jpg
+# Ensure that your Image.jpg file is in your working directory
+# You can modify the working directory using setwd(), e.g.
+# setwd("~/Desktop/MATH336/Project/")
 img <- readJPEG("Image.jpg") # Read the image
+img <- readJPEG("Image3.jpg")
 
 # Obtain Image dimension
 imgDm <- dim(img)
@@ -33,13 +29,96 @@ plotOriginal <- ggplot(data = imgRGB, aes(x = x, y = y)) +
 
 # Compress the image using k-means clustering 
 k <- 3 # Number of clusters, you can play with it to obtain different compression levels!
-kMeans <- kmeans(imgRGB[, c("R", "G", "B")], centers = k) 
-num.of.colours <- rgb(kMeans$centers[kMeans$cluster, ])
+#kMeans <- kmeans(imgRGB[, c("R", "G", "B")], centers = k) 
+kmeans1 = function(dataset, number_of_clusters){
+  #D <- imgRGB[, c("R", "G", "B")]
+  #k=3
+  #initialize
+  k <- number_of_clusters
+  D <- dataset
+  D_size <- nrow(D)
+  index <- sample(nrow(D),k)
+  centroids <- D[index, ]
+  #new_centroid <- matrix(ncol=3)
+  new_centroids <- data.frame(R=double(),G=double(),B=double())
+  
+  
+  #!centroids.equals(new_centroids)
+  while (!identical(centroids,new_centroids)){
+    # cluster is the centroid datapoint is assigned to
+    cluster = c()
+    for(i in 1:D_size){
+      # initialize empty label vector 
+      
+      # set min distance to the first centroid of that datapoint
+      min_dist = dist(rbind(centroids[1, ],D[i, ]), method = "euclidean")
+      #min_dist = sqrt(sum((centroids[1,]-D[i,])^2))
+      # the min distance set for centroid 1 as defult
+      cluster[i] = 1
+      # for each centroid...
+      for(j in 1:k){
+        # calc euclidean dist between it and the datapoints
+        current_dist = dist(rbind(centroids[j, ],D[i, ]), method = "euclidean")
+        # if its the smallest dist...
+        if(current_dist <= min_dist){
+          # update the min distance of that datapoint
+          min_dist = current_dist
+          # add the cluster to vector of labels corresponding to each datapoint
+          cluster[i] = j
+          #eg: 1,2,3,2,2,1,2,3
+        }
+      }
+    }
+    #head(cluster)
+    #tail(cluster)
+    
+    # update centroids if we have changed new centroid
+    #any(!is.na(new_centroid))
+    if(dim(new_centroids)[1] != 0){
+      centroids = new_centroids
+    }
+    
+    for(j in 1:k){
+      cPoints = c()
+      # index of the datapoints in cluster k
+      cPoints <- which(cluster %in% j)
+      
+      # set mean to be new centroid of cluster 
+      #new_centroid[k,] <- colMeans(x[cPoints,])
+      new_centroids[j, ] <- colMeans(D[cPoints,])
+    }
+    
+  }
+  
+  # only need to return clusters and centers(centroids)
+  km <- list(cluster = cluster,centers = centroids)
+  
+  return(km)
+}
+
+kmeans3 <- kmeans1(imgRGB[, c("R", "G", "B")],number_of_clusters =3)
+kmeans16 <- kmeans1(imgRGB[, c("R", "G", "B")],number_of_clusters =16)
+kmeans20 <- kmeans1(imgRGB[, c("R", "G", "B")],number_of_clusters =20)
+
+num.of.colours3 <- rgb(kmeans3$centers[kmeans3$cluster, ])
+num.of.colours16 <- rgb(kmeans16$centers[kmeans16$cluster, ])
+num.of.colours20 <- rgb(kmeans20$centers[kmeans20$cluster, ])
 
 # Obtain a plot of the compressed image
-plotCompressed <-ggplot(data = imgRGB, aes(x = x, y = y)) + 
-  geom_point(colour = num.of.colours) + 
-  labs(title = paste("k-Means Clustering of ", k, " Colours"))
+plotCompressed3 <-ggplot(data = imgRGB, aes(x = x, y = y)) + 
+  geom_point(colour = num.of.colours3) + 
+  labs(title = paste("k-Means Clustering of ", 3, " Colours"))
+
+plotCompressed16 <-ggplot(data = imgRGB, aes(x = x, y = y)) + 
+  geom_point(colour = num.of.colours16) + 
+  labs(title = paste("k-Means Clustering of ", 16, " Colours"))
+
+plotCompressed20 <-ggplot(data = imgRGB, aes(x = x, y = y)) + 
+  geom_point(colour = num.of.colours20) + 
+  labs(title = paste("k-Means Clustering of ", 20, " Colours"))
+
+
 
 # Plot the original and compressed image side by side
-grid.arrange(plotOriginal, plotCompressed, nrow = 2)  
+grid.arrange(plotOriginal, plotCompressed3, plotCompressed16,plotCompressed20,  nrow = 2)  
+
